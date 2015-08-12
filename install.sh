145edf0ba76e4f849d986ef69bc76a192801f825
dbritos <dbritos@gmail.com>
2015-08-12 19:39:00 -0300

    new

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 make#!/bin/sh
 apt-get install libsdl1.2debian
-wget -c http://download.virtualbox.org/virtualbox/5.0.0/virtualbox-5.0_5.0.0-101573~Ubuntu~trusty_amd64.deb
+if [! -f virtualbox-5.0_5.0.0-101573~Ubuntu~trusty_amd64.deb ];
+then
+   wget -c http://download.virtualbox.org/virtualbox/5.0.0/virtualbox-5.0_5.0.0-101573~Ubuntu~trusty_amd64.deb	
+fi
 dpkg --install virtualbox-5.0_5.0.0-101573~Ubuntu~trusty_amd64.deb
 apt-get install snmp
 tar -xf vde2-2.3.2-patch.tar
