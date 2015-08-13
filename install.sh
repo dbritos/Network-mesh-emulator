3a1c1db45d1629781b0d3bd4b1f762f6f08cbb9a
dbritos <dbritos@gmail.com>
2015-08-12 23:18:02 -0300

    new

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
-make#!/bin/sh
-apt-get -y install libsdl1.2debian
-if [! -f virtualbox-5.0_5.0.0-101573~Ubuntu~trusty_amd64.deb ];
-then
-   wget -c http://download.virtualbox.org/virtualbox/5.0.0/virtualbox-5.0_5.0.0-101573~Ubuntu~trusty_amd64.deb	
+#!/bin/sh
+file="virtualbox-5.0_5.0.0-101573~Ubuntu~trusty_amd64.deb"
+if [ -e ${PWD}/$file ];
+then echo "existe"   ${PWD}/$file
+else wget -c http://download.virtualbox.org/virtualbox/5.0.0/virtualbox-5.0_5.0.0-101573~Ubuntu~trusty_amd64.deb	
 fi
+apt-get -y install libsdl1.2debian
 dpkg --install virtualbox-5.0_5.0.0-101573~Ubuntu~trusty_amd64.deb
 apt-get -y install snmp
 tar -xf vde2-2.3.2-patch.tar
@@ -18,4 +19,8 @@ apt-get -y install python-gtk2-dev
 apt-get -y install python-gobject
 apt-get -y install python-cairo
 apt-get -y install python-netsnmp
+apt-get -y remove libvdeplug2
+apt-get -y install libvdeplug2
 VBoxManage import openwrtv12.ova
+
+
